fea917f3b15199bc857a2aff0c8bcfdc640a9b99
Rahul Ambhore <rahulambhore394@gmail.com>
2025-01-02 18:18:58 +0530

    Event module complete with database connectivity

--- app/src/main/java/com/focus/pragyaa/EventFragment.kt
+++ app/src/main/java/com/focus/pragyaa/EventFragment.kt
@@ -1,44 +1,74 @@
 package com.focus.pragyaa
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.focus.pragyaa.Utils.EventDataClass
 import com.focus.pragyaa.Utils.EventAdapter
-
+import com.focus.pragyaa.Utils.EventFirebaseDataClass
 import com.focus.pragyaa.databinding.FragmentEventBinding
+import com.google.firebase.database.*
 
 class EventFragment : Fragment() {
 
-    private var _binding: FragmentEventBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentEventBinding
+    private lateinit var database: DatabaseReference
+    private val eventList = mutableListOf<EventFirebaseDataClass>()
+    private lateinit var eventAdapter: EventAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentEventBinding.inflate(inflater, container, false)
+        binding = FragmentEventBinding.inflate(inflater, container, false)
+
+        database = FirebaseDatabase.getInstance().getReference("events")
+
+        setupRecyclerView()
+
+        fetchEvents()
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val sampleEvents = listOf(
-            EventDataClass("HackFusion 2.0", R.drawable.hackfusion, "20", "Feb"),
-            EventDataClass("TechFest", R.drawable.image1, "25", "Feb"),
-            EventDataClass("Tech Conference", R.drawable.image2, "28", "Feb")
-        )
-
+    private fun setupRecyclerView() {
         binding.eventRecyclerCardview.layoutManager = LinearLayoutManager(requireContext())
-        binding.eventRecyclerCardview.adapter = EventAdapter(sampleEvents)
+        eventAdapter = EventAdapter(eventList) { event ->
+            onEventClicked(event)
+        }
+        binding.eventRecyclerCardview.adapter = eventAdapter
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun fetchEvents() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                eventList.clear()
+                for (eventSnapshot in snapshot.children) {
+                    val event = eventSnapshot.getValue(EventFirebaseDataClass::class.java)
+                    event?.let { eventList.add(it) }
+                }
+                eventAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FirebaseError", error.message)
+            }
+        })
+    }
+
+    private fun onEventClicked(event: EventFirebaseDataClass) {
+        val intent = Intent(requireContext(), ViewEventDetailActivity::class.java).apply {
+            putExtra("eventName", event.eventName)
+            putExtra("coverImageUrl", event.coverImageUrl)
+            putExtra("eventDateDay", event.eventDateDay)
+            putExtra("eventDateMonth", event.eventDateMonth)
+            putExtra("eventDescription", event.eventDescription)
+            putExtra("eventRegistrationLink", event.eventRegistrationLink)
+        }
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/focus/pragyaa/ViewEventDetailActivity.kt
@@ -0,0 +1,44 @@
+package com.example.pragyaapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.focus.pragyaa.databinding.ActivityViewEventBinding
+
+
+class ViewEventDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewEventBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityViewEventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val eventName = intent.getStringExtra("eventName")
+        val coverImageUrl = intent.getStringExtra("coverImageUrl")
+        val eventDateDay = intent.getStringExtra("eventDateDay")
+        val eventDateMonth = intent.getStringExtra("eventDateMonth")
+        val eventDescription = intent.getStringExtra("eventDescription")
+        val eventRegistrationLink = intent.getStringExtra("eventRegistrationLink")
+
+        binding.viewEventEventName.text = eventName
+       // binding.eventDateTextView.text = "$eventDateDay $eventDateMonth"
+        binding.eventDescription.text = eventDescription
+
+
+        Glide.with(this)
+            .load(coverImageUrl)
+            .into(binding.viewEventCardImageview)
+
+        binding.btnRegisterEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(eventRegistrationLink))
+            startActivity(intent)
+        }
+    }
+}
